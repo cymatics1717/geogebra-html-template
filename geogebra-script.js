ac064d1aa22d8f920a427fa391f49a8b8129e522
cymatics <cymatics@foxmail.com>
2022-12-02 20:13:46 +0800

    first commit

---
+++ geogebra-script.js
@@ -0,0 +1,52 @@
+var parameters = {
+    "id": "ggbApplet",
+    "width": 2190,
+    "height": 1004,
+    "showMenuBar": true,
+    "showAlgebraInput": true,
+    "showToolBar": true,
+    "customToolBar": "0 73 62 | 1 501 67 , 5 19 , 72 75 76 | 2 15 45 , 18 65 , 7 37 | 4 3 8 9 , 13 44 , 58 , 47 | 16 51 64 , 70 | 10 34 53 11 , 24  20 22 , 21 23 | 55 56 57 , 12 | 36 46 , 38 49  50 , 71  14  68 | 30 29 54 32 31 33 | 25 17 26 60 52 61 | 40 41 42 , 27 28 35 , 6",
+    "showToolBarHelp": true,
+    "showResetIcon": false,
+    "enableLabelDrags": false,
+    "enableShiftDragZoom": true,
+    "enableRightClick": false,
+    "errorDialogsActive": false,
+    "useBrowserForJS": false,
+    "allowStyleBar": false,
+    "preventFocus": false,
+    "showZoomButtons": true,
+    "capturingThreshold": 3,
+    // add code here to run when the applet starts
+    "appletOnLoad": function(api) {
+        api.evalCommand("A = (1,1)");
+        api.evalCommand("B = (3,2)");
+        api.evalCommand("C = (6,-2)");
+        api.evalCommand("Circle(A,B,C)");
+        api.evalCommand("Segment(A, B)");
+        api.evalCommand("Segment(C, B)");
+        api.evalCommand("Segment(A, C)");
+        api.evalCommand("PerpendicularBisector(A, B)");
+        api.evalCommand("PerpendicularBisector(C, B)");
+        api.evalCommand("PerpendicularBisector(A, C)");
+    },
+    "showFullscreenButton": true,
+    "scale": 1,
+    "disableAutoScale": false,
+    "allowUpscale": false,
+    "clickToLoad": false,
+    "appName": "classic",
+    "buttonRounding": 0.7,
+    "buttonShadows": false,
+    "language": "en",
+    // use this instead of ggbBase64 to load a material from geogebra.org
+    // "material_id":"RHYH3UQ8",
+    // use this instead of ggbBase64 to load a .ggb file
+    // "filename":"myfile.ggb",
+
+};
+// is3D=is 3D applet using 3D view, AV=Algebra View, SV=Spreadsheet View, CV=CAS View, EV2=Graphics View 2, 
+//CP=Construction Protocol, PC=Probability Calculator DA=Data Analysis, FI=Function Inspector, macro=Macros
+var views = { 'is3D': 0, 'AV': 1, 'SV': 0, 'CV': 0, 'EV2': 0, 'CP': 0, 'PC': 0, 'DA': 0, 'FI': 0, 'macro': 0 };
+var applet = new GGBApplet(parameters, '5.0', views);
+window.onload = function() { applet.inject('ggbApplet') };
